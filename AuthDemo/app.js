65a7ffacb5add25fa977ad2a5470e9f4b062ceb0
Suneet <47031021+SuneetKumar19@users.noreply.github.com>
2019-07-07 22:53:55 +0530

    Update app.js

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,13 +1,25 @@
 var express = require("express");
+
+
 var mongoose = require("mongoose");
+
+
 var passport = require("passport");
+
 var bodyParser = require("body-parser");
+
 var LocalStrategy = require("passport-local");
+
 var passportLocalMongoose = require("passport-local-mongoose");
+
 mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
+
 var User = require("./models/user");
+
 var app = express();
+
 app.set('view engine', 'ejs');
+
 
 app.use(require("express-session")({
 
